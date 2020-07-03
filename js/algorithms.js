418d1ddd29d6f12ac9a35b092e56454e4e146e0f
rexliu3 <40341857+rexliu3@users.noreply.github.com>
2020-07-03 00:00:34 -0700

    Match by preferences working

--- js/algorithms.js
+++ js/algorithms.js
@@ -110,7 +110,7 @@ var seniors = [
             [0, "18:00", "19:00"],
         ],
         intEx: 50,
-        visits: 1,
+        visits: 0,
     },
     {
         name: "Walter",
@@ -120,7 +120,7 @@ var seniors = [
             [0, "15:00", "19:00"],
         ],
         intEx: 30,
-        visits: 1,
+        visits: 0,
     },
     {
         name: "Philomena",
@@ -130,7 +130,7 @@ var seniors = [
             [0, "17:00", "19:00"],
         ],
         intEx: 90,
-        visits: 0,
+        visits: 1,
     },
 ];
 
@@ -170,6 +170,7 @@ function getTimeOnWeekDay(type, name, weekday) {
     return returnTimes;
 }
 
+//TODO Repeats 3 Times
 function matchByAvailability(volunteers, seniors) {
     // Given 2 arrays, one of volunteers, and one of seniors, it returns array of objects of all possible matches
     // Addresses time
@@ -356,25 +357,8 @@ function matchByAvailability(volunteers, seniors) {
     return finals;
 }
 
-var results = matchByAvailability(volunteers, seniors);
-// for loop to print
-function printSeniorNameDayedFormat(results) {
-    //Iterate through the volunteers
-    for (var i = 0; i < results.length; i++) {
-        console.log(results[i].name);
-        //Iterate through the seniors matched with volunteers
-        for (var j = 0; j < results[i].matches.length; j++) {
-            console.log("     " + results[i].matches[j].name);
-            //Iterate through the times of the seniors
-            for (var k = 0; k < results[i].matches[j].times.length; k++) {
-                console.log("          " + results[i].matches[j].times[k][0], results[i].matches[j].times[k][1], results[i].matches[j].times[k][2]);
-            }
-
-        }
-    }
-}
-
 // -------------------------------------------------------
+
 function matchByNumVisits(finals, seniors) {
     // Returns the number of visits of the senior
     function getVisits(seniorName) {
@@ -431,9 +415,6 @@ function matchByNumVisits(finals, seniors) {
     return returningFinals;
 }
 
-var results = matchByNumVisits(results, seniors);
-console.log(printSeniorNameDayedFormat(results));
-
 
 // finals = [
 //     {name: "Rex", matches: [
@@ -447,7 +428,6 @@ console.log(printSeniorNameDayedFormat(results));
 //     ];
 
 function matchByPreference(finals) {
-
     function getIntExByName(role, name) {
         for (var i = 0; i < role.length; i++) {
             if (role[i].name === name) {
@@ -456,20 +436,20 @@ function matchByPreference(finals) {
             }
         }
     }
-
+// -------------------------------------------
     // For every volunteer in finals, find their index score using their name
 
     // 0630 note to self: need to clean up the formatting for final answer
     // Iterate through the volunteers
-    for (var i = 0; i < finals.length; i++) {
+    /*for (var i = 0; i < finals.length; i++) {
         var volunteerName = finals[i].name;
         var volunteerIntEx = getIntExByName(volunteers, volunteerName);
         var seniorPaired = "";
         var smallestDiff = 100;
         // Iterate through the matched seniors of a given volunteer
         for (var j = 0; j < finals[j].matches.length; j++) {
-            seniorIntEx = getIntExByName(seniors, finals[i].matches[j].name);
-            diff = Math.abs(volunteerIntEx - seniorIntEx);
+            var seniorIntEx = getIntExByName(seniors, finals[i].matches[j].name);
+            var diff = Math.abs(volunteerIntEx - seniorIntEx);
             if (diff < smallestDiff) {
                 smallestDiff = diff;
                 seniorPaired = finals[i].matches[j].name;
@@ -478,11 +458,86 @@ function matchByPreference(finals) {
         matches = [];
         matches.push();
     }
-    return seniorPaired;
+    //return seniorPaired;*/
+// -------------------------------------------
+
+    var returningFinals = [];
+    // Iterate through the volunteers
+    for (var i = 0; i < finals.length; i++) {
+        var volunteerTemplate = finals[i];
+        /* volunteerTemplate
+        {name: "Rex", matches: [
+                        {name: "Nancy", times: [[0, '13:30','16:45'],[4, '10:00','12:00']]}
+                         {name: "Walter", times: [[5, '13:00','14:00'],[6, '15:00','19:00']]}
+                   ] }
+        */
+        var seniorTemplate = finals[i].matches;
+        /* seniorTemplate
+            [
+                {name: "Nancy", times: [[0, '13:30','16:45'],[4, '10:00','12:00']]}
+                {name: "Walter", times: [[5, '13:00','14:00'],[6, '15:00','19:00']]}
+            ] 
+        */
+
+        // Array of the Senior Names who have the lowest visit numbers
+        var minimumSeniorsArr = [];
+        var minimumDiff = 1000;
+        var volunteerIntEx = getIntExByName(volunteers, volunteerTemplate.name);
+        // Iterate through the seniors for that volunteer
+        for (var j = 0; j < seniorTemplate.length; j++) {
+            var seniorName = seniorTemplate[j].name;
+            var seniorIntEx = getIntExByName(seniors, seniorName);
+            var diff = Math.abs(volunteerIntEx - seniorIntEx);
+            if (diff < minimumDiff) {
+                minimumDiff = diff;
+                minimumSeniorsArr = [];
+                minimumSeniorsArr.push(seniorName);
+            } else if (diff === minimumDiff) {
+                minimumSeniorsArr.push(seniorName);
+            }
+        }
+        var matches = [];
+        // Iterate through the seniors for that volunteer
+        for (var k = 0; k < seniorTemplate.length; k++) {
+            if (minimumSeniorsArr.includes(seniorTemplate[k].name)) {
+                matches.push({name:seniorTemplate[k].name, times:seniorTemplate[k].times});
+            }
+        }
+        returningFinals.push({ name: volunteerTemplate.name, matches: matches });
+        
+    }
+    return returningFinals;
 }
-    // For every senior in matches, find their intex score using their name
+
+function printSeniorNameDayedFormat(results) {
+    //Iterate through the volunteers
+    for (var i = 0; i < results.length; i++) {
+        console.log(results[i].name);
+        //Iterate through the seniors matched with volunteers
+        for (var j = 0; j < results[i].matches.length; j++) {
+            console.log("     " + results[i].matches[j].name);
+            //Iterate through the times of the seniors
+            for (var k = 0; k < results[i].matches[j].times.length; k++) {
+                console.log("          " + results[i].matches[j].times[k][0], results[i].matches[j].times[k][1], results[i].matches[j].times[k][2]);
+            }
+
+        }
+    }
+}
+
+
+var results = matchByAvailability(volunteers, seniors);
+results = matchByNumVisits(results, seniors);
+results = matchByPreference(results);
+console.log(printSeniorNameDayedFormat(results));
+
+// for loop to print
+
+
+
+    // For every senior in matches, find their index score using their name
     // Create variable called smallest diff
-    // Calculate the difference in intex score between volunteer and senior
+    // Calculate the difference in index score between volunteer and senior
     // If smaller than smallest diff, seniorwithsmallestdiff = diff !! this is the senior's name. 
 
     // Return the match in the format of volunteer = [
